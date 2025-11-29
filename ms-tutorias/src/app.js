1e40cef4ecd375462f06e66856a279a0661dd20f
adria <adrian29_virgo@hotmail.com>
2025-11-29 09:51:33 -0500

    instalando nuevas dependencias

--- ms-tutorias/src/app.js
+++ ms-tutorias/src/app.js
@@ -5,8 +5,15 @@ const config = require('./config'); // Importamos nuestra configuración central
 const tutoriasRouter = require('./api/routes/tutorias.routes');
 const errorHandler = require('./api/middlewares/errorHandler'); // El manejador de errores reutilizable
 const correlationIdMiddleware = require('./api/middlewares/correlationId.middleware.js');
+// Importamos la librería prom-client para métricas Prometheus
+const client = require('prom-client');
 
 const app = express();
+
+// Configuración de métricas Prometheus
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
 
 // Middlewares esenciales
 app.use(express.json()); // Permite al servidor entender y procesar bodies en formato JSON
@@ -19,6 +26,12 @@ app.use('/tutorias', tutoriasRouter);
 // Middleware de manejo de errores
 // Debe ser el ÚLTIMO middleware que se añade.
 app.use(errorHandler);
+
+// Endpoint para métricas Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 // Iniciar el servidor
 app.listen(config.port, () => {
